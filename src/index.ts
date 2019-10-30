f6bdbebb5048828af8332ae3d992117b6d03ec47
Tom Parsons <thomaspatrickparsons@gmail.com>
2019-10-30 11:21:34 +0000

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser"
-import express from "express"
+import express, {NextFunction, Request, Response} from "express"
 
 import routes from "./handlers/rest"
 
@@ -10,6 +10,13 @@ const {PORT = 5000} = process.env
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(routes)
+
+app.use(function(_: Request, res: Response, next: NextFunction) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
+
 
 app.listen(PORT, () => {
   // tslint:disable-next-line no-console
