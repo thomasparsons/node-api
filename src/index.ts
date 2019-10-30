ecc08815997d91094edb29d486d4a6dc3aa44357
Tom Parsons <thomaspatrickparsons@gmail.com>
2019-10-30 11:25:16 +0000

    try this

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,15 @@ const app = express()
 
 const {PORT = 5000} = process.env
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(routes)
-
 app.use(function(_: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next();
-});
+  next()
+})
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(routes)
 
 app.listen(PORT, () => {
   // tslint:disable-next-line no-console
